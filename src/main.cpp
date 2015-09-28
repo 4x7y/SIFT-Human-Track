419fdd7cb783eedd2420ce69a4fa4ae05499ea9e
yuechuan <xueyuechuan@gmail.com>
2015-09-28 16:10:00 +0800

    Second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <cv.h>
 #include <highgui.h>
 
 int main(int argc, char const* argv[])
 {
-    std::cout << "Hello World!\n";
+    const cv::Mat input = cv::imread("input.jpg", 0); //Load as grayscale
+
+    cv::SiftFeatureDetector detector;
+    std::vector<cv::KeyPoint> keypoints;
+    detector.detect(input, keypoints);
+
+    // Add results to image and save.
+    cv::Mat output;
+    cv::drawKeypoints(input, keypoints, output);
+    cv::imwrite("sift_result.jpg", output);
+
     return 0;
 }
+
+
